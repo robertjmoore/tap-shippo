c5b74f50006aed9c7af38812185bb4fd8aa1c711
Mike DeLaurentis <miked@stitchdata.com>
2017-02-26 20:48:31 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(name='tap-shippo',
       version='0.1.0',
-      description='Singer Tap for Shippo',
+      description='Singer.io tap for extracting data from the Shippo API',
       author='Robert J. Moore',
-      url='https://github.com/robertjmoore/tap-shippo',
+      url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_shippo'],
-      install_requires=['stitchstream-python>=0.6.0',
+      install_requires=['stitchstream-python>=0.7.1',
                         'requests==2.12.4'],
       entry_points='''
           [console_scripts]
