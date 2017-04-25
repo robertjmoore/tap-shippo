9ec75c6f06650255ba65eea6e1ad3582fd03d31a
Mike DeLaurentis <miked@stitchdata.com>
2017-04-25 07:23:30 -0400

    0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-shippo',
-      version='0.5.2',
+      version='0.6.0',
       description='Singer.io tap for extracting data from the Shippo API',
       author='Robert J. Moore',
       url='http://singer.io',
